3d73a50bf65ca3ec2b3cb569a7ccd8260ce111e8
Robert Wang <rwang5688@outlook.com>
2021-08-14 20:44:24 +0000

    add list-cofig-rules-default.sh

---
+++ sample/list-config-rules-default.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd default
+list-config-rules.sh default-eu-west-1.json
+list-config-rules.sh default-us-east-1.json
+list-config-rules.sh default-us-east-2.json
+list-config-rules.sh default-us-west-2.json
+cd ..
